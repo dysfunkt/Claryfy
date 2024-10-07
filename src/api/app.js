41e79b5bc554a885f3df561d5f1d6ab72fa786d1
Willy Tang Jing Lin <willytjl@gmail.com>
2024-10-08 00:52:09 +0800

    backend vercel fix #2

--- src/api/app.js
+++ src/api/app.js
@@ -559,10 +559,10 @@ app.post('/reset-password', (req, res) => {
 
 /* HELPER METHODS */
 
-app.get('/test', (req, res) => {
-    res.send("success")
-})
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 })
+
+module.exports = app;
